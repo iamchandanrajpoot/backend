454240b71e9a4783c957a4a754b0bf5769672acf
iamchandanrajpoot <iamchandanrajpoot@gmail.com>
2024-03-03 14:02:57 +0530

    add const {requestListner, dummyFunction} = require("./routes") as comment

--- writingAndFromFile/cleaningCode.js
+++ writingAndFromFile/cleaningCode.js
@@ -2,6 +2,7 @@ const http = require("http");
 const routes = require("./routes");
 const server = http.createServer(routes);
 
+// const {requestListner, dummyFunction} = require("./routes")
 // routes.requestListner
 // routes.dummyFunction
 
