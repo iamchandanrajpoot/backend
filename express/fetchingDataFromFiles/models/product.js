38c7efbf2d1298aeb9ef555de24891b0b5d1fc61
iamchandanrajpoot <iamchandanrajpoot@gmail.com>
2024-03-05 10:28:01 +0530

    resolve problem of unnecessay reading file two times

---
+++ express/fetchingDataFromFiles/models/product.js
@@ -0,0 +1,55 @@
+const { json } = require("body-parser");
+const fs = require("fs");
+const path = require("path");
+
+module.exports = class Product {
+  constructor(t) {
+    this.title = t;
+  }
+
+  // save() {
+  //   const p = path.join(
+  //     path.dirname(process.mainModule.filename),
+  //     "data",
+  //     "products.json"
+  //   );
+  //   fs.readFile(p, (err, fileContent) => {
+  //     let products = [];
+  //     if (!err) {
+  //       products = JSON.parse(fileContent);
+  //     }
+  //     products.push(this);
+  //     fs.writeFile(p, JSON.stringify(products), (err) => {
+  //       console.log(err);
+  //     });
+  //   });
+  // }
+
+  save() {
+    Product.fetchAll((products) => {
+      const p = path.join(
+        path.dirname(process.mainModule.filename),
+        "data",
+        "products.json"
+      );
+      products.push(this);
+      fs.writeFile(p, JSON.stringify(products), (err) => {
+        console.log(err);
+      });
+    });
+  }
+
+  static fetchAll(cb) {
+    const p = path.join(
+      path.dirname(process.mainModule.filename),
+      "data",
+      "products.json"
+    );
+    fs.readFile(p, (err, fileContent) => {
+      if (err) {
+        cb([]);
+      }
+      cb(JSON.parse(fileContent));
+    });
+  }
+};
