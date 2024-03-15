c859a427f507bf85c5f714ae326c192250bde3ba
iamchandanrajpoot <iamchandanrajpoot@gmail.com>
2024-03-15 16:16:47 +0530

    update login controller

--- expense-tacker-2.0-APIs/controlles/userControllers.js
+++ expense-tacker-2.0-APIs/controlles/userControllers.js
@@ -1,4 +1,5 @@
 const User = require("../models/userModel");
+const { use } = require("../routes/userRoutes");
 
 exports.postRegister = async (req, res) => {
   try {
@@ -6,28 +7,34 @@ exports.postRegister = async (req, res) => {
       where: { email: req.body.email },
     });
     if (isAlreadyRegister) {
-      console.log("already a user")
-      res.json({ message: `already register with this email ${req.body.email}`});
+      console.log("already a user");
+      res.json({
+        message: `already register with this email ${req.body.email}`,
+      });
     } else {
       await User.create(req.body);
       res.json({ message: "succussfully register" });
     }
   } catch (error) {
-    console.log(error); 
+    console.log(error);
     res.json({ message: "internal server error" });
   }
 };
 
-exports.postLogin = async (req, res, next) => {
+exports.postLogin = async (req, res) => {
   try {
     const { email, password } = req.body;
     const user = await User.findOne({ where: { email: email } });
-    if (!user || user.password !== password) {
-      res.status(401).json({ message: "Invalalid email or password" });
+    if (!user) {
+      res.status(404).json({ message: "User not found" });
     } else {
+      if (password !== user.password) {
+        res.status(401).json({ message: "User not authorized" });
+      }
+    }
+    if (user && user.password == password) {
       console.log(user);
       res.status(200).json({ message: "successfully login" });
-      // next();
     }
   } catch (error) {
     console.log(error);
