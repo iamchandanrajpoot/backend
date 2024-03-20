6cf5409cbdd52a00d5f7e3d90afab3dd817d579f
iamchandanrajpoot <iamchandanrajpoot@gmail.com>
2024-03-20 08:46:00 +0530

    use transaction for post , delete, put operations

--- expense-tacker-2.0-APIs/controlles/expenseController.js
+++ expense-tacker-2.0-APIs/controlles/expenseController.js
@@ -1,26 +1,38 @@
 // const Expense = require("../models/expenseModel");
+const sequelize = require("../config/dbConfig");
 const User = require("../models/userModel");
 
 exports.postExpense = async (req, res) => {
+  const t = await sequelize.transaction();
   try {
-    console.log("i am comming inside post request");
     const userInstance = await User.findByPk(req.user.id);
     const { expendicture, description, category } = req.body;
     if (!expendicture || !description || !category) {
       res.json({ message: "Please enter all fields" });
     }
-    const expense = await userInstance.createExpense({
-      expendicture,
-      description,
-      category,
-    });
-    await userInstance.update({
-      totalExpense: parseInt(userInstance.totalExpense) + parseInt(expendicture),
-    });
-    res.status(201).json(expense);
+    const expense = await userInstance.createExpense(
+      {
+        expendicture,
+        description,
+        category,
+      },
+      { transaction: t }
+    );
+    await userInstance.update(
+      {
+        totalExpense:
+          parseInt(userInstance.totalExpense) + parseInt(expendicture),
+      },
+      { transaction: t }
+    );
+    await t.commit();
+    res.status(201).json({ successful: true, expense });
   } catch (error) {
-    console.log(error);
-    res.status(500).json({ message: "interanl server error" });
+    t.rollback();
+    // console.log(error);
+    res
+      .status(500)
+      .json({ successful: false, message: "interanl server error" });
   }
 };
 
@@ -49,21 +61,22 @@ exports.getExpenseById = async (req, res) => {
   }
 };
 exports.deleteExpenseById = async (req, res) => {
+  const t = await sequelize.transaction();
   try {
     const userInstance = await User.findByPk(req.user.id);
     const expenses = await userInstance.getExpenses({
       where: { id: req.params.expenseId },
     });
+    const updatedExpense =
+      parseInt(userInstance.totalExpense) - parseInt(expenses[0].expendicture);
+    await expenses[0].destroy({transaction: t});
+    await userInstance.update({ totalExpense: updatedExpense }, {transaction: t});
 
-    const deleteResponse = await expenses[0].destroy();
-
-    if (deleteResponse) {
-      return res.json({ message: "Deleted" });
-    } else {
-      return res.status(500).json({ message: "Failed to delete expense" });
-    }
+    await t.commit()
+    return res.json({ message: "Deleted" });
   } catch (error) {
-    console.log(error);
+    await t.rollback();
+    // console.log(error);
     res.status(500).json({ message: "internal server error" });
   }
 };
